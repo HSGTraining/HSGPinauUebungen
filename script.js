088c187bce8ca6b44a238196bc59d990d4a67568
HSGTraining <noah-hoppe@freenet.de>
2024-06-14 14:58:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const exercises = [
-    { id: 1, category: 'U10', name: 'Dribbling Übung', image: 'dribbling.png' },
-    { id: 1, category: 'U10', name: 'Wurftraining', image: 'wurftraining.png' },
-    { id: 2, category: 'U12', name: 'Cirkel Training', image: 'wurftraining.png' },
+    { id: 1, category: 'U10', name: 'Dribbling Übung', image: 'dribling.png' },
+    { id: 2, category: 'U10', name: 'Wurftraining', image: 'wurftraining.png' },
+    { id: 3, category: 'U14', name: 'Cirkel Training', image: 'wurftraining.png' },
     // Weitere Übungen hinzufügen
 ];
 
